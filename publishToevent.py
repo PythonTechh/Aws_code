45a12b9f27b209819cde263592da1fc7cee0815a
PythonTechh <105942950+PythonTechh@users.noreply.github.com>
2023-05-31 01:23:17 -0400

    Update publishToevent.py

--- publishToevent.py
+++ publishToevent.py
@@ -6,16 +6,34 @@ def lambda_handler(event, context):
     # TODO implement
     print(event)
     client= boto3.client('events')
-    response = client.create_event_bus(
-    Name='travel_data_by_code',
-    Tags=[
-        {
-            'Key': 'sealid',
-            'Value': '110397'
-        },
-    ]
-)
+#     response = client.create_event_bus(
+#     Name='travel_data_by_code',
+#     Tags=[
+#         {
+#             'Key': 'sealid',
+#             'Value': '110397'
+#         },
+#     ]
+# )
     
+#     response = client.put_rule(
+#     Name='Event_rule_by_code',
+#     EventPattern='{"detail": {\
+#     "source": ["orxe", "maui"],\
+#     "object": ["orders", "transactions"]\
+#   }}'
+# ,
+#     State='ENABLED',
+#     Description='Event_rule_by_code',
+#     #,RoleArn='string',
+#     Tags=[
+#         {
+#             'Key': 'sealid',
+#             'Value': '110397'
+#         },
+#     ],
+#     EventBusName='traveleventbus'
+# )
     response = client.put_events(
     Entries=[{
             'Time': datetime(2015, 1, 1),
